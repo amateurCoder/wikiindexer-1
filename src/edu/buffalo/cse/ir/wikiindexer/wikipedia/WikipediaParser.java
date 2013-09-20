bca90464116f172f628d528a9da0d8c3223e2cf6
Nikhil Khandelwal <nk.7189@gmail.com>
2013-09-20 18:56:01 -0400

    regex

--- src/edu/buffalo/cse/ir/wikiindexer/wikipedia/WikipediaParser.java
+++ src/edu/buffalo/cse/ir/wikiindexer/wikipedia/WikipediaParser.java
@@ -87,8 +87,8 @@ public class WikipediaParser {
 	 */
 	public static String parseTagFormatting(String text) {
 		if(null!=text){
-//			return text.replaceAll("[^(<[^>]*> )( </{0,1}[^>]*/{0,1}> )( </{0,1}[^>]*>)$]","");
-//			return text.replaceAll("[(^<[^>]*> )( <[^>]*>)( <[^>]>)]","");
+		return text.replaceAll("<[^>]*> ?| ?<.*?>|&lt;([^.]*?)&gt;", "");
+		
 		}
 		return null;
 	}
@@ -117,9 +117,188 @@ public class WikipediaParser {
 	 *  The 0th element is the parsed text as visible to the user on the page
 	 *  The 1st element is the link url
 	 */
-	public static String[] parseLinks(String text) {
-		return null;
+	public static String[] parseLinks(String text) 
+	{
+		String tempStr[] = new String[2];
+		tempStr[0]="";
+		tempStr[1]="";
+		
+		if(null!=text)
+		{
+			
+			 if(text.matches("\\[\\[[0-9A-Za-z \\|0-9A-Za-z ]+\\]\\]"))
+			 {
+	  	      String temp[]=text.split("\\|");
+	  		  tempStr[0]=temp[1].replaceAll("\\[\\[|\\]\\]", "");
+	  		  tempStr[1]=temp[0].replaceAll("\\]\\]|\\[\\[", "");
+	  		  
+			 }
+			
+	  	 
+	  	 	//for link with stupid (brackets)
+			if(text.matches("\\[+.* \\(*.*\\)*\\|\\]+"))
+			{
+				//stupid wikipedisa:case1
+				if(text.matches("\\[+Wikipedia:.* *\\|\\]+"))
+				{
+					//stupid case2
+					if(text.matches("\\[+Wikipedia:.* *\\(+.*\\)+\\|\\]+"))
+					{
+			  	      	
+						String temp[]=text.split("\\:");
+						String temp2[]=temp[1].split("\\(");
+				  		tempStr[0]= temp2[0].trim();
+			  	 		
+				  		tempStr[1]="";
+				  		return tempStr;
+					
+					}
+					
+					if(text.matches("\\[+Wikipedia:.*\\#[A-Za-z0-9 _]\\|\\]+"))
+					{
+			  	      	
+						
+				  		tempStr[0]= text.replaceAll("\\[|\\||\\]", "");			  	 			  	 		
+				  		tempStr[1]="";
+				  		return tempStr;
+					
+					}
+					String temp[]=text.split("\\:");
+			  		tempStr[0]= temp[1].replaceAll("\\[|\\||\\]", "");
+		  	 		
+			  		tempStr[1]="";
+			  		return tempStr;
+				
+				}
+				
+				
+				 	 		
+	  	 		tempStr[1]=text.replaceAll("\\[|\\]|\\|", "");
+		  		tempStr[1]= tempStr[1].replaceAll(" ", "_");
+		  		tempStr[1]=Character.toUpperCase(tempStr[1].charAt(0)) + tempStr[1].substring(1);
+		  		String temp[]=text.split("\\ ");
+		  		tempStr[0]= temp[0].replaceAll("\\[|,", "");
+		  	
+			}
+			
+			if(text.matches("\\[+.*:.*\\|*\\]+"))
+			{
+				if(text.matches("\\[+Wiktionary:.*\\|*\\]+"))
+				{
+		  	      	 
+					String temp[]=text.split("\\|");
+				
+			  		tempStr[0]= temp[0].replaceAll("\\[|\\]|\\|","");
+		  	 		
+			  		tempStr[1]="";
+			  		return tempStr;
+				
+				}
+				
+				if(text.matches("\\[+.*:.*\\|.*\\]+"))
+				{
+		  	      	 
+					String temp[]=text.split("\\|");
+					
+					if(temp.length==0)
+					{
+						tempStr[0]="";
+					}
+					else
+					{
+						tempStr[0]= temp[temp.length-1].replaceAll("\\]", "");
+					}
+			  		tempStr[1]="";
+			  		return tempStr;
+				
+				}
+				if(text.matches("\\[+.*:.*\\]+"))
+				{
+		  	      	 
+					String temp[]=text.split("\\|");
+				
+			  		tempStr[0]= "";
+		  	 		
+			  		tempStr[1]="";
+			  		return tempStr;
+				
+				}
+			}
+				
+			//for stupid links with auto capitaliztion 
+			
+			if(text.matches(".* \\[+.*\\]+.*"))
+				 {
+				if(text.matches(".* \\[+.*\\|.*\\]+.*"))
+				 {
+					String tempStr2="";
+					  Pattern pattern = Pattern.compile("\\[\\[.*\\]\\]");
+					  Matcher matcher = pattern.matcher(text);
+				  	  while (matcher.find()) 
+				  	  {
+				  		  String temp[]=matcher.group(0).split("\\|");
+				  		  tempStr[1]=temp[0];
+				  		tempStr2=temp[1].replaceAll("\\]", ""); //to hold public transportation
+				  	  }
+				  	  
+				  	  	tempStr[1]= tempStr[1].replaceAll(" ", "_");
+				  		tempStr[1]= tempStr[1].replaceAll("\\[|\\]", "");
+				  		
+				  		tempStr[1]=Character.toUpperCase(tempStr[1].charAt(0)) + tempStr[1].substring(1);
+				  		
+			  		  
+			  		  String temp[]=text.split("\\[\\[");
+			  		  tempStr[0]=temp[0]+tempStr2;
+			  		  
+			  		  
+			  		 return tempStr;
+					
+					
+				 }
+				  //Pattern pattern = Pattern.compile("\\[\\[[0-9A-Za-z ]+\\]\\]");
+				  Pattern pattern = Pattern.compile("\\[\\[.*\\]\\]");
+			  	  Matcher matcher = pattern.matcher(text);
+			  	  while (matcher.find()) 
+			  	  {
+			  		tempStr[1]= matcher.group(0).replaceAll(" ", "_");
+			  	  }
+			  		tempStr[1]= tempStr[1].replaceAll("\\[|\\]", "");
+			  		
+			  		tempStr[1]= tempStr[1].replaceAll("<nowiki \\/>", "");
+			  		tempStr[1]=Character.toUpperCase(tempStr[1].charAt(0)) + tempStr[1].substring(1);
+			  		
+		  		  
+		  		  tempStr[0]=text.replaceAll("\\[|\\]", "");
+		  		tempStr[0]= tempStr[0].replaceAll("<nowiki \\/>", "");
+		  		tempStr[0]= tempStr[0].replaceAll("\\.", "");
+		  		 return tempStr;
+		  	 	  }
+		
+			
+	
+			
+			
+			//for link with stupid 
+			
+			
+			
+			
+			
+			
+			}
+	  		  
+	  		 
+		
+	  	 
+		
+		
+		
+			 
+			return tempStr;
 	}
+		
+
+		
 
 	public WikipediaDocument getWikipediaDocument() {
 		String section = null;
